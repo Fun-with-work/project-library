0c26361d90e6579a7fdb8956898a56ee49807deb
aaman <aneela.aman.is@gmail.com>
2024-11-21 17:54:59 -0500

    add variables for the form

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const form = document.getElementById("form");
-
+const title = document.getElementById ("title");
+const author = document.getElementById ("author");
+const pages = document.getElementById ("pages");
+const bookRead = document.getElementById ("book-read");
 
 
 const myLibrary = [];
@@ -9,7 +12,6 @@ function Book(title, author, pages, read, like) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.like = like;
     this.info = function() {
         return(this.title +" by " + this.author + ", " + this.pages + ", " + this.read +", " + this.like + ".")
     }
