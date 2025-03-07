807f48dc8f088c7332249517c472bd80c5ce41bb
aaman <aneela.aman.is@gmail.com>
2025-03-07 10:15:08 -0500

    add prevent default for submit button

--- script.js
+++ script.js
@@ -10,8 +10,14 @@ let displayTitle = document.getElementsByClassName("display-title");
 let displayAuthor = document.getElementsByClassName("display-author");
 let displayPages = document.getElementsByClassName("display-pages");
 let displayRead = document.getElementsByClassName("display-read");
+const readStatus = document.createElement("button");
 
-const myLibrary = [];
+let myLibrary = [];
+
+submitButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("The form can not be submitted.")
+});
 
 let title;
 let author;
@@ -21,7 +27,7 @@ let read;
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = pages + " pages";
     this.read = read;
     return;
 }
@@ -41,28 +47,27 @@ function addBookToLibrary(title, author, pages, read) {
         pagesInput.textContent = "";
         readInput.textContent = "";
     }
-    
 
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
     book = "";
+    return;
 };
-
-// addBookToLibrary();
-
-
 
 submitButton.addEventListener("click", event => {
     addBookToLibrary(title, author, pages, read);
 });
 
 
-const book1 = new Book('The Hobbit', "J. R. R. Tolkien", '295 pages', 'not read yet');
-const book2 = new Book('To Kill a Mockingbird', 'Harper Lee', '281', 'not read yet');
-const book3 = new Book('The Count of Monte Cristo', 'Alexandre Dumas', '1312 pages', 'not read yet');
-const book4 = new Book('Great Expectations', 'Charles Dickens', '544 pages', 'not read yet');
-const book5 = new Book('Crime and Punishment', 'Fyodor Dostoevsky', '527 pages', 'not read yet');
+let book1 = new Book('The Hobbit', "J. R. R. Tolkien", '295', 'read');
 
+let book2 = new Book('To Kill a Mockingbird', 'Harper Lee', '281', 'read');
+
+let book3 = new Book('The Count of Monte Cristo', 'Alexandre Dumas', '1312', 'not read yet');
+
+let book4 = new Book('Great Expectations', 'Charles Dickens', '544', 'read');
+
+let book5 = new Book('Crime and Punishment', 'Fyodor Dostoevsky', '527 pages', 'not read yet');
 
 myLibrary.push(book1);
 myLibrary.push(book2);
@@ -71,18 +76,17 @@ myLibrary.push(book4);
 myLibrary.push(book5);
 
 console.log(myLibrary);
-// let n = 0;
 
-function bookPageDisplay (displayTitle, displayAuthor, displayPages) {
-    for (n=0; n <5; n++) {
-        displayTitle[n].textContent = (myLibrary[n].title);
-        displayAuthor[n].textContent = (myLibrary[n].author);
-        displayPages[n].textContent = (myLibrary[n].pages);  
-        // n  += 1; 
+function bookPageDisplay(displayTitle, displayAuthor, displayPages, displayRead) {
+    for (i = 0; i <= myLibrary.length; i++) {
+        displayTitle[i].textContent = (myLibrary[i].title);
+        displayAuthor[i].textContent = (myLibrary[i].author);
+        displayPages[i].textContent = (myLibrary[i].pages);
+        displayRead[i].textContent = (myLibrary[i].read);
     }
 }
 
-bookPageDisplay(displayTitle, displayAuthor, displayPages);
+bookPageDisplay(displayTitle, displayAuthor, displayPages, displayRead);
 
 
 
